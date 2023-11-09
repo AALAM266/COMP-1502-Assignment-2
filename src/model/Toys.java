af57a73c440aeea50ad15b28dffdad7b2796d415
Akheel Alameddin <aalam266@mtroyal.ca>
2023-11-08 17:09:14 -0700

    Added toy classes

---
+++ src/model/Toys.java
@@ -0,0 +1,38 @@
+package model;
+
+public abstract class Toys {
+	
+	private String Figures;
+	private String Animals;
+	private String Puzzles;
+	private String BoardGames;
+	
+	public String getFigures() {
+		return Figures;
+	}
+	public void setFigures(String figures) {
+		Figures = figures;
+	}
+	public String getAnimals() {
+		return Animals;
+	}
+	public void setAnimals(String animals) {
+		Animals = animals;
+	}
+	public String getPuzzles() {
+		return Puzzles;
+	}
+	public void setPuzzles(String puzzles) {
+		Puzzles = puzzles;
+	}
+	public String getBoardGames() {
+		return BoardGames;
+	}
+	public void setBoardGames(String boardGames) {
+		BoardGames = boardGames;
+	}
+
+	
+	
+
+}

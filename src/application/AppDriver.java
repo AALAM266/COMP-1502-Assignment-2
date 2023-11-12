d0fe6fc7acf718d5ed585793047c37b8728575c5
Akheel Alameddin <aalam266@mtroyal.ca>
2023-11-11 23:22:53 -0700

    Completed documentation of the application package

--- src/application/AppDriver.java
+++ src/application/AppDriver.java
@@ -2,9 +2,18 @@ package application;
 
 import controller.AppController;
 
-
+/**
+ * The starting point of the project and will be used to run the application
+ * @authors: Akheel Alam Eddin and Keegan Hong
+ * @version: 1.0
+ */
 public class AppDriver {
 
+	/**
+	 * The main method of the project
+	 * @param args 
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		
 		new AppController();
